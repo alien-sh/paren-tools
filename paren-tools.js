4ca49bf81c010c22d42e30cff6642f495fab3710
Pouya Eghbali <persian.writer@gmail.com>
2019-12-28 00:04:57 +0100

    Bug fixes

--- paren-tools.js
+++ paren-tools.js
@@ -1,30 +1,33 @@
 module.exports = core => {
-  core.repl.keyEaters['('] = [
-    function (key) {
-      this.insertAtCursor(key)
-      this.insertAtCursor(')')
-      this.clear()
-      this.x--
-      this.preprint()
-      this.print()
+  core.repl.keyEaters["("] = [
+    function(key) {
+      if (this.isBusy) return;
+      this.insertAtCursor(key);
+      this.insertAtCursor(")");
+      this.clear();
+      this.x--;
+      this.preprint();
+      this.print();
     }
-  ]
+  ];
   core.repl.keyEaters[core.keys.backspace] = [
-    function (key) {
-      const index = this.y * this.size() + this.x
-      const currChar = this.currentInput[index - 1]
-      const nextChar = this.currentInput[index]
-      if ((currChar == '(') && (nextChar == ')')) {
-        this.x++
-        this.insertAtCursor(key)
+    function(key) {
+      if (this.isBusy) return;
+      const index = this.y * this.size() + this.x;
+      const currChar = this.currentInput[index - 1];
+      const nextChar = this.currentInput[index];
+      if (currChar == "(" && nextChar == ")") {
+        this.x++;
+        this.insertAtCursor(key);
       }
-      this.insertAtCursor(key)
+      this.insertAtCursor(key);
     }
-  ]
-  core.onBeforeProcess.push(function (src) {
-    if (!src.startsWith('(') && !src.endsWith(')')) {
-      return `(${src})`
+  ];
+  core.onBeforeProcess.push(function(src) {
+    if (this.isBusy) return;
+    if (!src.startsWith("(") && !src.endsWith(")")) {
+      return `(${src})`;
     }
-    return src
-  })
-}
+    return src;
+  });
+};
